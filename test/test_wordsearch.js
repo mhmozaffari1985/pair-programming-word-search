4af7e6124dd1f3f8045bddfe55ad004902f6cfcf
mh.mozaffari <mh.mozaffari1985@gmail.com>
2021-07-04 20:29:12 -0400

    complete

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,36 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'R', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'A', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'N', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'K', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK')
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word is not present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'R', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'A', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'S', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'K', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ILAN')
+
+    assert.isFalse(result);
+  });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,18 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
+    let lettersV = letters[0].map((item, colIndex) => letters.map(row => row[colIndex]));
+    const verticalJoin = lettersV.map(ls => ls.join(''))
+
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true
+    }
+
+    return false;
+
 }
 
 module.exports = wordSearch
